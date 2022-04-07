794f80a31d574e4e22c7a67da868aa1e9aa69ff1
ParkSangSun <appleasz1028102@gmail.com>
2022-04-07 17:51:31 +0900

    Create : ViewPager2 + TabLayout

--- app/src/main/java/com/pss/meimfacttask/MainActivity.kt
+++ app/src/main/java/com/pss/meimfacttask/MainActivity.kt
@@ -2,10 +2,28 @@ package com.pss.meimfacttask
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.google.android.material.tabs.TabLayoutMediator
+import com.pss.meimfacttask.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private val tabTitleArray = arrayOf(
+        "Home",
+        "Favorite"
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        val mainViewPager = binding.viewPager
+        val mainTabLayout = binding.tabLayout
+
+        mainViewPager.adapter = MainViewPagerAdapter(supportFragmentManager, lifecycle)
+
+        TabLayoutMediator(mainTabLayout, mainViewPager) { tab, position ->
+            tab.text = tabTitleArray[position]
+        }.attach()
     }
 }
