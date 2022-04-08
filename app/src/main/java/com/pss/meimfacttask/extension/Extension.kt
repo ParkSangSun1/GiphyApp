6cc8ba4103d733f43b7fd86af96ed21e79dfdd31
ParkSangSun <appleasz1028102@gmail.com>
2022-04-08 21:07:01 +0900

    Update : RecyclerView 표시 아이템을 가로 2개씩 세로로 표현

--- app/src/main/java/com/pss/meimfacttask/extension/Extension.kt
+++ app/src/main/java/com/pss/meimfacttask/extension/Extension.kt
@@ -1,10 +1,18 @@
 package com.pss.meimfacttask.extension
 
 import android.content.Context
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
 
 //Vertical RecyclerView
 fun RecyclerView.showVertical(context: Context){
     this.layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+}
+
+fun RecyclerView.showGrid(context: Context){
+    layoutManager = GridLayoutManager(
+        context, 2
+    )
 }
--- app/src/main/java/com/pss/meimfacttask/view/MainFragment.kt
+++ app/src/main/java/com/pss/meimfacttask/view/MainFragment.kt
@@ -3,9 +3,11 @@ package com.pss.meimfacttask.view
 import android.util.Log
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.pss.meimfacttask.R
 import com.pss.meimfacttask.adapter.GiphyListAdapter
 import com.pss.meimfacttask.databinding.FragmentMainBinding
+import com.pss.meimfacttask.extension.showGrid
 import com.pss.meimfacttask.extension.showVertical
 import com.pss.meimfacttask.viewmodel.MainViewModel
 import kotlinx.coroutines.launch
@@ -20,9 +22,9 @@ class MainFragment : BaseFragment<FragmentMainBinding>(R.layout.fragment_main) {
 
     private fun initRecyclerView(){
         giphyListAdapter = GiphyListAdapter(requireContext())
-
         binding.recyclerView.adapter = giphyListAdapter
-        binding.recyclerView.showVertical(requireContext())
+        binding.recyclerView.showGrid(requireContext())
+        binding.recyclerView.setHasFixedSize(true)
 
 
         lifecycleScope.launch {
