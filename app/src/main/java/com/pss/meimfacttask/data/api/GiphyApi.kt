4f63cb0c5f77b59e793f236fe9f7b89ff140d8e5
ParkSangSun <appleasz1028102@gmail.com>
2022-04-08 09:11:15 +0900

    Add : Giphy api 호출

---
+++ app/src/main/java/com/pss/meimfacttask/data/api/GiphyApi.kt
@@ -0,0 +1,20 @@
+package com.pss.meimfacttask.data.api
+
+import com.pss.meimfacttask.Utils.END_URL
+import com.pss.meimfacttask.Utils.GIPHY_API_KEY
+import com.pss.meimfacttask.data.model.GiphyResponse
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface GiphyApi {
+
+    @GET(END_URL)
+    suspend fun getGiphyGifs(
+        @Query("api_key") key: String = GIPHY_API_KEY,
+        @Query("limit") limit: Int,
+        @Query("offset") offset: Int,
+        @Query("lang") lang: String = "ko",
+        @Query("bundle") bundle: String = "clips_grid_picker"
+    ): Response<GiphyResponse>
+}
